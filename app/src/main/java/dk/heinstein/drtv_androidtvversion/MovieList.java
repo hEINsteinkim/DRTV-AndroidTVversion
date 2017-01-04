cb510ce0996acd287eca519a3f8a1c9ac9da308d
hEINstein <heinstein@gmail.com>
2017-01-04 21:27:37 +0100

    test jason, test single mp4

--- app/src/main/java/dk/heinstein/drtv_androidtvversion/MovieList.java
+++ app/src/main/java/dk/heinstein/drtv_androidtvversion/MovieList.java
@@ -3,7 +3,28 @@ package dk.heinstein.drtv_androidtvversion;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 public final class MovieList {
+
+    static InputStream is = null;
+    private static JSONObject json = null;
+    static String output = "";
+
     public static final String MOVIE_CATEGORY[] = {
             "Category Zero",
             "Category One",
@@ -14,6 +35,7 @@ public final class MovieList {
     };
 
     public static List<Movie> list;
+
 
     public static List<Movie> setupMovies() {
         list = new ArrayList<Movie>();
@@ -33,7 +55,7 @@ public final class MovieList {
                 + "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat.";
 
         String videoUrl[] = {
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4",
+                "http://drod09m-vh.akamaihd.net/p/all/clear/download/24/585bad8aa11fa012c869c024/Langt-fra-Borgen--Folkekirken_06331f59002141dbb66592dcf19cf68c_2974.mp4",
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search.mp4",
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
@@ -54,7 +76,34 @@ public final class MovieList {
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
         };
 
-        list.add(buildMovieInfo("category", title[0],
+        /*try {
+            JSONObject json = getJSONFromUrl("http://www.dr.dk/mu-online/api/1.3/list/view/news?limit=20");
+            JSONArray items = json.getJSONArray("Items");
+
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject ProgramCard = items.getJSONObject(i);
+
+                Movie movie= buildMovieInfo();
+                ProgramCard.getString("Urn")
+
+
+
+
+                /*Video video = new Video(videoObject.getString("title"),
+                        videoObject.getString("description"),
+                        videoObject.getJSONObject("player")
+                                .getString("default"),
+                        videoObject.getJSONObject("thumbnail")
+                                .getString("sqDefault"));
+                                */
+  /*              list.add(movie);
+            }
+
+        } catch (JSONException e) {
+            // manage exceptions
+        }
+*/
+       list.add(buildMovieInfo("category", title[0],
                 description, "Studio Zero", videoUrl[0], cardImageUrl[0], bgImageUrl[0]));
         list.add(buildMovieInfo("category", title[1],
                 description, "Studio One", videoUrl[1], cardImageUrl[1], bgImageUrl[1]));
@@ -66,6 +115,51 @@ public final class MovieList {
                 description, "Studio Four", videoUrl[4], cardImageUrl[4], bgImageUrl[4]));
 
         return list;
+    }
+
+    public static JSONObject getJSONFromUrl(String url, List params) {
+        URL _url;
+        HttpURLConnection urlConnection;
+
+        try {
+            _url = new URL(url);
+            urlConnection = (HttpURLConnection) _url.openConnection();
+        }
+        catch (MalformedURLException e) {
+            Log.e("JSON Parser", "Error due to a malformed URL " + e.toString());
+            return null;
+        }
+        catch (IOException e) {
+            Log.e("JSON Parser", "IO error " + e.toString());
+            return null;
+        }
+
+        try {
+            is = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder total = new StringBuilder(is.available());
+            String line;
+            while ((line = reader.readLine()) != null) {
+                total.append(line).append('\n');
+            }
+            output = total.toString();
+        }
+        catch (IOException e) {
+            Log.e("JSON Parser", "IO error " + e.toString());
+            return null;
+        }
+        finally{
+            urlConnection.disconnect();
+        }
+
+        try {
+            json = new JSONObject(output);
+        }
+        catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        return json;
     }
 
     private static Movie buildMovieInfo(String category, String title,
